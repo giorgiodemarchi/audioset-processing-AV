a008ea354848fa13345e02443db4ecb0cce49147
giorgiodemarchi <giorgio_demarchi@yahoo.com>
2024-03-31 11:15:36 -0400

    bug fix: start time was wrong

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ from utils.aws_utils import already_in_dataset
 from utils.utils import download_video, upload_metadata_to_s3, upload_video_to_s3, free_local_memory
 
 if __name__ == '__main__':
-    S3_DEST_DIR_KEY = "original_eval/"
+    S3_DEST_DIR_KEY = "eval_strong/"
     SET = 'eval'
 
     # AWS Access
@@ -18,36 +18,39 @@ if __name__ == '__main__':
     df = pd.read_csv(f'audioset_data/audioset_{SET}_strong.tsv', sep='\t')
     segments_ids = df.segment_id.unique()
     j=0
-    for video_url in segments_ids:
-        
+    for i, video_url in enumerate(segments_ids):
 
-        try:
-            video_df = df[df['segment_id']==video_url]
-            start_time = video_df.start_time_seconds.min()
-            end_time = video_df.end_time_seconds.max()
+        if i > 1000:
+            try:
+                video_df = df[df['segment_id']==video_url]
+                start_time = int(video_url.split('_')[-1])/1000
+                end_time = start_time + video_df.end_time_seconds.max()
+                
+                already_present, length = already_in_dataset(video_url, bucket_name, S3_DEST_DIR_KEY, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
+
+                if j%100==0:
+                    print(f"{length} points in dataset.")
+
+                if already_present == 0:
+                    exit_code, local_file = download_video(video_url, start_time, end_time)
+
+                    if exit_code == 0:
+                        # Put objects on S3
+                        s3_path = S3_DEST_DIR_KEY + str(video_url) + "/"
+                        upload_video_to_s3(local_file, s3_path, bucket_name, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
+                        upload_metadata_to_s3(video_df, s3_path, bucket_name, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
+
+                        # Free local space
+                        free_local_memory(local_file)
+                        
+                        j+=1
+                else:
+                    print('Already present')
+
+            except Exception as e:
+                print(f"Download/Upload failed for id: {video_url}")
+                print(e)
             
-            already_present, length = already_in_dataset(video_url, bucket_name, S3_DEST_DIR_KEY, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
 
-            if j%100==0:
-                print(f"{length} points in dataset.")
-
-            if already_present == 0:
-                exit_code, local_file = download_video(video_url, start_time, end_time)
-
-                if exit_code == 0:
-                    # Put objects on S3
-                    s3_path = "original_eval/" + str(video_url) + "/"
-                    upload_video_to_s3(local_file, s3_path, bucket_name, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
-                    upload_metadata_to_s3(video_df, s3_path, bucket_name, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
-
-                    # Free local space
-                    free_local_memory(local_file)
-            else:
-                print('Already present')
-        except Exception as e:
-            print(f"Download/Upload failed for id: {video_url}")
-            print(e)
-        j+=1 
-
-        if j > 1000:
+        if j > 5000:
             break
